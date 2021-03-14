3e16e81dae0083b1dcec6814b0264064a424b91a
emersonsiega <emersonsiega@hotmail.com>
2021-03-14 13:16:23 -0300

    Ensure MinLengthValidation returns error if value is invalid

--- test/validation/validators/min_length_validation_test.dart
+++ test/validation/validators/min_length_validation_test.dart
@@ -4,10 +4,17 @@ import 'package:clean_architecture_proposal/validation/validation.dart';
 
 class MinLengthValidation implements FieldValidation {
   final String field;
-  MinLengthValidation(this.field);
+  final int minLength;
+  MinLengthValidation(this.field, this.minLength);
 
   @override
   String validate(String value) {
+    if (value?.isNotEmpty == true) {
+      if (value.length < minLength) {
+        return "Invalid value. Min length is $minLength";
+      }
+    }
+
     return null;
   }
 }
@@ -16,7 +23,7 @@ void main() {
   MinLengthValidation sut;
 
   setUp(() {
-    sut = MinLengthValidation('any_field');
+    sut = MinLengthValidation('any_field', 3);
   });
 
   test('Should return null if value is empty', () {
@@ -29,5 +36,11 @@ void main() {
     final error = sut.validate(null);
 
     expect(error, isNull);
+  });
+
+  test('Should return error if value is invalid', () {
+    final error = sut.validate('ab');
+
+    expect(error, 'Invalid value. Min length is 3');
   });
 }
