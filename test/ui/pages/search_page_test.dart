dd455723b1c02698488040a42c694646c2e581c4
emersonsiega <emersonsiega@hotmail.com>
2021-03-14 10:15:39 -0300

    Ensure SearchPage loads with correct values

---
+++ test/ui/pages/search_page_test.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class SearchPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    void _hideKeyboard() {
+      FocusScope.of(context).requestFocus(FocusNode());
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lyrics Search"),
+        centerTitle: true,
+      ),
+      body: GestureDetector(
+        onTap: _hideKeyboard,
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 32),
+          child: Form(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Artist",
+                    hintText: "Eric Clapton",
+                    prefixIcon: Icon(Icons.person),
+                    errorText: null,
+                  ),
+                  textInputAction: TextInputAction.none,
+                ),
+                const SizedBox(height: 30),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Music",
+                    hintText: "Tears in Heaven",
+                    prefixIcon: Icon(Icons.music_note),
+                    errorText: null,
+                  ),
+                  textInputAction: TextInputAction.done,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.search),
+        onPressed: null,
+      ),
+    );
+  }
+}
+
+void main() {
+  testWidgets('Should load SearchPage with correct values',
+      (WidgetTester tester) async {
+    final page = MaterialApp(home: SearchPage());
+    await tester.pumpWidget(page);
+
+    final artistInputTextChildren = find.descendant(
+      of: find.bySemanticsLabel('Artist'),
+      matching: find.byType(Text),
+    );
+    expect(
+      artistInputTextChildren,
+      findsNWidgets(2),
+      reason: "Should find just two text children: Label and Hint text.",
+    );
+
+    final musicInputTextChildren = find.descendant(
+      of: find.bySemanticsLabel('Music'),
+      matching: find.byType(Text),
+    );
+    expect(
+      musicInputTextChildren,
+      findsNWidgets(2),
+      reason: "Should find just two text children: Label and Hint text.",
+    );
+
+    final button = tester.widget<FloatingActionButton>(
+      find.byType(FloatingActionButton),
+    );
+    expect(button.onPressed, null);
+  });
+}
