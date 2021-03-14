392330944c6f263781bf5992a126fb3af37637f1
emersonsiega <emersonsiega@hotmail.com>
2021-03-14 13:28:24 -0300

    Move duplicated code to setUp method

--- test/validation/validators/validation_composite_test.dart
+++ test/validation/validators/validation_composite_test.dart
@@ -18,16 +18,32 @@ class ValidationComposite implements Validation {
 class FieldValidationSpy extends Mock implements FieldValidation {}
 
 void main() {
+  ValidationComposite sut;
+  FieldValidationSpy validation1;
+  FieldValidationSpy validation2;
+  FieldValidationSpy validation3;
+
+  void mockValidation(FieldValidationSpy validation,
+      {String field: 'any_field', String error}) {
+    when(validation.field).thenReturn(field);
+    when(validation.validate(any)).thenReturn(error);
+  }
+
+  setUp(() {
+    validation1 = FieldValidationSpy();
+    mockValidation(validation1);
+
+    validation2 = FieldValidationSpy();
+    mockValidation(validation2);
+
+    validation3 = FieldValidationSpy();
+    mockValidation(validation3, field: 'other_field');
+
+    sut = ValidationComposite([validation1, validation2]);
+  });
+
   test('Should return null if all validations returns null or empty', () {
-    final validation1 = FieldValidationSpy();
-    when(validation1.field).thenReturn('any_field');
-    when(validation1.validate(any)).thenReturn(null);
-
-    final validation2 = FieldValidationSpy();
-    when(validation2.field).thenReturn('any_field');
-    when(validation2.validate(any)).thenReturn('');
-
-    final sut = ValidationComposite([validation1, validation2]);
+    mockValidation(validation2, error: '');
 
     final error = sut.validate(field: 'any_field', value: 'any_value');
 
