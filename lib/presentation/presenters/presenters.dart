36b0c4dec53a36f8e01254de92921b6e2608cf0e
emersonsiega <emersonsiega@hotmail.com>
2021-03-15 08:54:18 -0300

    Adds RxDart presenter implementation and test

--- lib/presentation/presenters/presenters.dart
+++ lib/presentation/presenters/presenters.dart
@@ -1,1 +1,2 @@
 export 'stream_lyrics_search_presenter.dart';
+export 'rxdart_lyrics_search_presenter.dart';
---
+++ lib/presentation/presenters/rxdart_lyrics_search_presenter.dart
@@ -0,0 +1,91 @@
+import 'dart:async';
+
+import 'package:clean_architecture_proposal/domain/domain.dart';
+import 'package:meta/meta.dart';
+import 'package:rxdart/subjects.dart';
+
+import '../../ui/ui.dart';
+
+import '../presentation.dart';
+
+class RxDartLyricsSearchPresenter implements LyricsSearchPresenter {
+  final Validation validation;
+  final LyricsSearch lyricsSearch;
+  final _state = LyricsSearchState();
+  final _stateController = BehaviorSubject<LyricsSearchState>();
+
+  RxDartLyricsSearchPresenter({
+    @required this.validation,
+    @required this.lyricsSearch,
+  }) {
+    _stateController.add(_state);
+  }
+
+  @override
+  Stream<String> get artistErrorStream =>
+      _stateController.stream.map((state) => state.artistError).distinct();
+
+  @override
+  Stream<String> get musicErrorStream =>
+      _stateController.stream.map((state) => state.musicError).distinct();
+
+  @override
+  Stream<bool> get isFormValidStream =>
+      _stateController.stream.map((state) => state.isFormValid).distinct();
+
+  @override
+  Stream<bool> get isLoadingStream =>
+      _stateController.stream.map((state) => state.isLoading).distinct();
+
+  @override
+  Stream<String> get localErrorStream =>
+      _stateController.stream.map((state) => state.localError).distinct();
+
+  @override
+  Stream<PageConfig> get navigateToStream =>
+      _stateController.stream.map((state) => state.navigateTo).distinct();
+
+  @override
+  Future<void> search() async {
+    try {
+      _state.isLoading = true;
+      _state.localError = null;
+      _state.navigateTo = null;
+      _update();
+
+      final entity = await lyricsSearch.search(
+        LyricsSearchParams(artist: _state.artist, music: _state.music),
+      );
+
+      _state.navigateTo = PageConfig('/lyric', arguments: entity);
+    } on DomainError catch (error) {
+      _state.localError = error.description;
+    } finally {
+      _state.isLoading = false;
+      _update();
+    }
+  }
+
+  void _update() => _stateController.add(_state);
+
+  @override
+  void validateArtist(String artist) {
+    _state.artist = artist;
+    final error = validation.validate(field: 'artist', value: artist);
+    _state.artistError = error;
+    _update();
+  }
+
+  @override
+  void validateMusic(String music) {
+    _state.music = music;
+    final error = validation.validate(field: 'music', value: music);
+    _state.musicError = error;
+    _update();
+  }
+
+  @override
+  void dispose() {
+    _stateController.close();
+  }
+}
