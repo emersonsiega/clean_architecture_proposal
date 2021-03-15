a5cd94407063049cb737c630de09fe80f3f91d9b
emersonsiega <emersonsiega@hotmail.com>
2021-03-15 13:54:47 -0300

    Ensure StreamLyricPresenter emits error when SaveFavoriteLyrics fails

--- test/presentation/presenters/stream_lyric_presenter_test.dart
+++ test/presentation/presenters/stream_lyric_presenter_test.dart
@@ -1,21 +1,49 @@
+import 'dart:async';
+
 import 'package:clean_architecture_proposal/domain/domain.dart';
+import 'package:clean_architecture_proposal/ui/ui.dart';
 import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 import 'package:meta/meta.dart';
 
-abstract class LyricPresenter {
+abstract class LyricPresenter implements LocalErrorManager {
   Future<void> addFavorite(LyricEntity entity);
+
+  void dispose();
+}
+
+class LyricState {
+  String localError;
 }
 
 class StreamLyricPresenter implements LyricPresenter {
   final SaveFavoriteLyrics saveFavoriteLyrics;
+  final _state = LyricState();
+  final _stateController = StreamController<LyricState>.broadcast();
 
   StreamLyricPresenter({@required this.saveFavoriteLyrics});
 
   @override
+  Stream<String> get localErrorStream =>
+      _stateController.stream.map((state) => state.localError).distinct();
+
+  @override
   Future<void> addFavorite(LyricEntity entity) async {
-    await saveFavoriteLyrics.save([entity]);
+    try {
+      await saveFavoriteLyrics.save([entity]);
+    } on DomainError catch (error) {
+      _state.localError = error.description;
+    } finally {
+      _update();
+    }
+  }
+
+  void _update() => _stateController.add(_state);
+
+  @override
+  void dispose() {
+    _stateController.close();
   }
 }
 
@@ -40,5 +68,16 @@ void main() {
     await sut.addFavorite(entity);
 
     verify(saveFavoriteLyricsSpy.save([entity])).called(1);
+  });
+
+  test('Should emits error if SaveFavoriteLyrics fails', () async {
+    when(saveFavoriteLyricsSpy.save(any)).thenThrow(DomainError.unexpected);
+
+    expectLater(
+      sut.localErrorStream,
+      emits('Something wrong happened. Please, try again!'),
+    );
+
+    await sut.addFavorite(entity);
   });
 }
